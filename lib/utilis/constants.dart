8b06f43b790741d390836faf962019b93af8f4de
TuanPham <tuan.pham@ad24.cz>
2021-12-22 22:45:41 +0100

    initial commit

---
+++ lib/utilis/constants.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+const color_black = Color.fromRGBO(48, 47, 47, 1.0);
+const color_grey = Color.fromRGBO(141, 141, 141, 1.0);
+
+// ignore: constant_identifier_names
+const texttheme_default = TextTheme(
+    headline1: TextStyle(
+        color: color_black, fontWeight: FontWeight.w700, fontSize: 26),
+    headline2: TextStyle(
+        color: color_black, fontWeight: FontWeight.w700, fontSize: 22),
+    headline3: TextStyle(
+        color: color_black, fontWeight: FontWeight.w700, fontSize: 20),
+    headline4: TextStyle(
+        color: color_black, fontWeight: FontWeight.w700, fontSize: 16),
+    headline5: TextStyle(
+        color: color_black, fontWeight: FontWeight.w700, fontSize: 14),
+    headline6: TextStyle(
+        color: color_black, fontWeight: FontWeight.w700, fontSize: 12),
+    bodyText1: TextStyle(
+        color: color_black,
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+        height: 1.5),
+    bodyText2: TextStyle(
+        color: color_grey,
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+        height: 1.5),
+    subtitle1: TextStyle(
+        color: color_black, fontSize: 12, fontWeight: FontWeight.w400),
+    subtitle2: TextStyle(
+        color: color_grey, fontSize: 12, fontWeight: FontWeight.w400));
+
+// ignore: constant_identifier_names
+const texttheme_small = TextTheme(
+    headline1: TextStyle(
+        color: color_black, fontWeight: FontWeight.w700, fontSize: 22),
+    headline2: TextStyle(
+        color: color_black, fontWeight: FontWeight.w700, fontSize: 20),
+    headline3: TextStyle(
+        color: color_black, fontWeight: FontWeight.w700, fontSize: 18),
+    headline4: TextStyle(
+        color: color_black, fontWeight: FontWeight.w700, fontSize: 14),
+    headline5: TextStyle(
+        color: color_black, fontWeight: FontWeight.w700, fontSize: 12),
+    headline6: TextStyle(
+        color: color_black, fontWeight: FontWeight.w700, fontSize: 10),
+    bodyText1: TextStyle(
+        color: color_black,
+        fontSize: 12,
+        fontWeight: FontWeight.w500,
+        height: 1.5),
+    bodyText2: TextStyle(
+        color: color_grey,
+        fontSize: 12,
+        fontWeight: FontWeight.w500,
+        height: 1.5),
+    subtitle1: TextStyle(
+        color: color_black, fontSize: 10, fontWeight: FontWeight.w400),
+    subtitle2: TextStyle(
+        color: color_grey, fontSize: 10, fontWeight: FontWeight.w400));
